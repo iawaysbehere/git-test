cd589789ad5b7c5bdbc184f2725e3b3e03ba78ee
iawaysbehere <62866532+iawaysbehere@users.noreply.github.com>
2021-04-27 17:07:41 +0800

    first branch commit

--- src/com/lexis/GitTest.java
+++ src/com/lexis/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello are you ok git");
         System.out.println("hello are you ok github");
+        System.out.println("hello are you ok branch hot-fix");
+
     }
 }
